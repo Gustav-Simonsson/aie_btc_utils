d7431cc2d10fe3137d1c4a5208e87788b886629d
Dirk Jaeckel <github@dirk.jaeckel.name>
2014-08-03 17:07:53 +0200

    Added t2hash to IncompleteT2AResponse

--- src/main/java/io/aie_btc_service/aie_btc_service/model/IncompleteT2AResponse.java
+++ src/main/java/io/aie_btc_service/aie_btc_service/model/IncompleteT2AResponse.java
@@ -1,11 +1,15 @@
 package io.aie_btc_service.aie_btc_service.model;
 
 import com.google.bitcoin.core.Sha256Hash;
+import com.google.bitcoin.core.Transaction;
+import com.google.bitcoin.params.MainNetParams;
 import io.aie_btc_service.aie_btc_service.IncompleteT2WithHash;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.xml.bind.DatatypeConverter;
+
+import static io.aie_btc_service.aie_btc_service.FullClient.NETWORK_PARAMETERS;
 
 public class IncompleteT2AResponse {
 
@@ -28,6 +32,17 @@ public class IncompleteT2AResponse {
         this.t2SigHashFlag = t2SigHashFlag;
         this.t2SigHashFlaganyonecanpay = t2SigHashFlaganyonecanpay;
         this.t2Raw = t2Raw;
+        this.t2Hash = getT2Hash(t2Raw);
+
+        Log.info("t2Hash: " + t2Hash);
+    }
+
+    private String getT2Hash(String t2Raw) {
+
+        byte[] t2RawBytes = DatatypeConverter.parseHexBinary(t2Raw);
+        Log.info("t2Raw: " + t2Raw);
+        Transaction t2 = new Transaction(NETWORK_PARAMETERS, t2RawBytes);
+        return t2.getHashAsString();
     }
 
     public IncompleteT2AResponse(IncompleteT2WithHash incompleteT2WithHash) {
@@ -42,6 +57,7 @@ public class IncompleteT2AResponse {
     private String t2SigHashFlag;
     private String t2SigHashFlaganyonecanpay;
     private String t2Raw;
+    private String t2Hash;
 
     public String getT2SigHashInput0() {
         return t2SigHashInput0;
